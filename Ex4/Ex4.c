2552dac5e8c73648d3559e1feecb2d7ba0f34201
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-07 14:25:43 +0200

    Solution to exercice 4

--- Ex4/Ex4.c
+++ Ex4/Ex4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(void) {
+    
+    int C;
+    printf("Podaj temperature w Celsjuszach: "); //24
+    scanf("%d", &C);
+    printf("\n");
+    
+    float F, K, N;
+    F = (C * 1.8) + 32;
+    K = C + 273.15;
+    N = C * 0.33;
+    
+    printf("F = %.2f\nK = %.2f\nN = %.2f\n", F, K, N);
+    
+    return 0;
+}
