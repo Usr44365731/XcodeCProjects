968181cff1addc186f05c11d8b49cc0491b9a683
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-07 14:16:19 +0200

    Adding solution to Exercise 3

--- Ex3/Ex3.c
+++ Ex3/Ex3.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main(void) {
+    
+    float dSpd, uSpd; //500MB
+    printf("Podaj predkosc pobierania w (Mbit/s): "); //302.14
+    scanf("%f", &dSpd);
+    printf("Podaj predkosc wysylania w (Mbit/s): "); //100.3
+    scanf("%f", &uSpd);
+    printf("\n");
+    
+    float MBdSpd, MBuSpd;
+    MBdSpd = dSpd / 8;
+    MBuSpd = uSpd / 8;
+    
+    float SdSpd, SuSpd;
+    SdSpd = 500 / MBdSpd;
+    SuSpd = 500 / MBuSpd;
+    
+    printf("Czas pobrania pliku wielkosci 500.00 MB to %.2f sekund\nCzas wyslania pliku wielkosci 500.00 MB to %.2f sekund\n", SdSpd, SuSpd);
+    
+    return 0;
+}
