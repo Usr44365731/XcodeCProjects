49824be4ff2f391c2d0039526fce41123a23dbdb
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-07 13:50:20 +0200

    Minor changes to Ex1 a added solution to Ex2

--- Ex1/Ex1.c
+++ Ex1/Ex1.c
@@ -3,17 +3,17 @@
 int main(void) {
     
     int a, b;
-    printf("Podaj pierwsza liczbe: ");
+    printf("Podaj pierwsza liczbe: "); //2
     scanf("%d", &a);
-    printf("Podaj droga liczbe: \n");
+    printf("Podaj droga liczbe: \n"); //-3
     scanf("%d", &b);
     printf("\n");
     
     int Sum = a+b, Roz = a-b, Mno = a*b;
     
-    printf("Suma: %d\n", Sum);
-    printf("Roznica: %d\n", Roz);
-    printf("Wynik mnozenia: %d\n\n", Mno);
+    printf("Suma: %d\n", Sum); //-1
+    printf("Roznica: %d\n", Roz); //5
+    printf("Wynik mnozenia: %d\n\n", Mno); //-6
     
     printf("Suma: %d\nRoznica: %d\nWynik mnozenia: %d\n", Sum, Roz, Mno);
     
--- Ex2/Ex2.c
+++ Ex2/Ex2.c
@@ -4,8 +4,18 @@ int main(void) {
     
     int H, M, S;
     
-    printf("Podaj godzine w formacie HH;MM:SS: ");
+    printf("Podaj godzine w formacie HH:MM:SS: "); //12:45:39
     scanf("%d: %d: %d", &H, &M, &S);
+    int Sum = H*3600+M*60+S;
+    int Number = Sum, DigitSum = 0;
+    
+    while (Number > 0) {
+        DigitSum += Number % 10;
+        Number /= 10;
+    }
+    
+    printf("Od godziny 00:00:00 uplynelo: %d sekund\n", Sum); //45939
+    printf("Suma cyfr z liczby %d rowna jest %d\n", Sum, DigitSum); // 30
     
     return  0;
 }
