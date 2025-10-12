377273c9a2135f5ea8e17bdb5a180821482bf520
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-12 21:05:36 +0200

    Added completed exercises from Ex5 to Ex 14 plus started Ex 15 and default template for Ex16

---
+++ Ex15/Ex15.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Ex15
+//
+//  Created by Kacper Kuczyński on 12/10/2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
