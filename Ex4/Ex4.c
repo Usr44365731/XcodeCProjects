4177d00a37946b095cea53d11e8c1f72610fcd45
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-13 11:31:34 +0200

    Fixed Ex4

--- Ex4/Ex4.c
+++ Ex4/Ex4.c
@@ -3,7 +3,8 @@
 int main(void) {
     
     //"\"Moj\"\n"
-    printf("\"Moj\"\n""\"drogi\"\n""\"program\"\n""\"w programie\"\n""\"Dante\"\n""\"na\"\n""\"platformie\"\n""\"online\"\n");
+    //printf("\"Moj\"\n""\"drogi\"\n""\"program\"\n""\"w programie\"\n""\"Dante\"\n""\"na\"\n""\"platformie\"\n""\"online\"\n");
+    printf("\"Moj\"\n""\"drogi\"\n""\"program\"\n""\"wprogramie\"\n""\"Dante\"\n");
     
     return 0;
 }
