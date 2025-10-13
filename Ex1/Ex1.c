fc21943c8aac1d425612f28266df696e702e5d77
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-13 10:21:26 +0200

    Added Ex 1

--- Ex1/Ex1.c
+++ Ex1/Ex1.c
@@ -2,7 +2,35 @@
 
 int main(void) {
     
-    printf("Moj pierwszy program\n");
+    int a;
+    printf("Podaj liczbę punktów: ");
+    scanf("%d", &a);
+    
+    if(a > 90 && a < 101) {
+        
+        printf("Piatka\n");
+    }
+    else if (a > 70 && a < 91) {
+        
+        printf("Czworka\n");
+    }
+    else if (a > 50 && a < 71) {
+        
+        printf("Trojka\n");
+    }
+    else if (a > 0 && a < 51) {
+        
+        printf("Dwojka\n");
+    }
+    else if (a < 0 || a > 100) {
+        
+        printf("Nieprawidłowa liczba punktów\n");
+    }
+    
+    else {
+        
+        printf("Podano niepoprawne dane\n");
+    }
     
     return 0;
 }
--- Ex2/Ex2.c
+++ Ex2/Ex2.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
 
-int main(void) {
-    
-    printf("Moj\ndrogi\nprogram\nw programie\nDante\nna\nplatformie\nonline\n");
-    
-    return 0;
-}
