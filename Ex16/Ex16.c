2bb5ffde3d28d41bf92a438b6594be6fa51fc71c
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-14 22:58:33 +0200

    Added Ex16 - Ex19

--- Ex16/Ex16.c
+++ Ex16/Ex16.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
     
-    
+    double a, b, c; //18, -819, -721
+    printf("Value of a: ");
+    scanf("%lf", &a);
+    printf("Value of b: ");
+    scanf("%lf", &b);
+    printf("Value of c: ");
+    scanf("%lf", &c);
+    printf("%.2lf\n", sqrt(pow(a, 2)+pow(b, 2)+pow(c, 2)));
     
     return 0;
 }
