db0fb50bf69a9607016eb0e54bdd13410a632e37
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-15 11:42:54 +0200

    Draft edit

---
+++ 15-10-25-A/15-10-25-A.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  15-10-25-A
+//
+//  Created by Kacper Kuczyński on 15/10/2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
