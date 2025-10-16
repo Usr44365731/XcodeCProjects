6bbc5b2a80d3c2626ca03e1c2650f9c00c2732bb
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-16 13:50:49 +0200

    Started Ex23

---
+++ Ex23/Ex23.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    
+    char num[9], num1[3], num2[3], num3[3]; //022086620
+    printf("Input 3 numbers in a single line: ");
+    scanf("%c", &num);
+    
+    return 0;
+}
