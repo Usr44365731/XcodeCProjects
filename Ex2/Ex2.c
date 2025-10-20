8b4a336a5f570428107cb1e2a7b1cab4e1308a32
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-20 10:26:12 +0200

    Resolving conflicts 2

--- Ex2/Ex2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    
-    printf("Moj\ndrogi\nprogram\nw programie\nDante\nna\nplatformie\nonline\n");
-    
-    return 0;
-}
--- Ex3/Ex3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    
-    printf("Moj\ndrogi\nprogram\nw programie\nDante\nna\nplatformie\nonline\nw jednej\nlinijce\n");
-    
-    return 0;
-}
